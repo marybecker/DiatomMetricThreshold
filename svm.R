69eb7071c50554d9c0b41ca2a6bf39b771f43009
marybecker <mary.becker@ct.gov>
2018-09-25 16:38:59 -0400

    tuned svm

--- svm.R
+++ svm.R
@@ -7,7 +7,7 @@ IND <-  read.csv ("DiatomMetrics_GAM.csv",sep=",",header=TRUE)
 IND<-IND[IND$ID!='S18882_2015',]
 IND[is.na(IND)] <- 0
 IND$TPGrp<-factor(ifelse(IND$TP_MGL<0.065,"low","high"))
-IND$TPGrp3<-factor(ifelse(IND$TP_MGL<0.04,"low",ifelse(IND$TP_MGL>0.065,"high","medium")))
+IND$TPGrp3<-factor(ifelse(IND$TP_MGL<=0.02,"low",ifelse(IND$TP_MGL>0.065,"high","medium")))
 IND$R<-(IND$H*10)/(IND$H+IND$L)
 IND$SH<-IND$H*10
 
@@ -22,8 +22,10 @@ y<-IND[,8]
 m<-IND[,c(5,6,8)]
 
 ##Run Linear SVM Model
-model<-svm(TPGrp~.,data=m,kernel="linear",cost=1,scale=FALSE,class.weights=(c("high"=4,"low"=1))) 
-pred<-predict(model,m)
+model<-svm(TPGrp~.,data=m,kernel="linear",cost=1,scale=FALSE,
+           class.weights=(c("high"=4,"low"=1)),probability=TRUE) 
+
+pred<-predict(model,m,probability = TRUE)
 table(pred=pred,true=m$TPGrp)# confusion matrix
 
 ##Run Test Dataset
@@ -31,6 +33,27 @@ m2<-INDT[,c(5,6,7)]
 pred2<- predict(model,m2)
 table(pred=pred2,true=m2$TPGrp)# confusion matrix
 
+# ##Run Linear SVM Model with Bootstrap
+# 
+# n<-2
+# lineoutput<- matrix(nrow=n,ncol=4)
+# confoutput<- matrix(nrow=n,ncol=4)
+# 
+# for(k in 1:n) {
+# 
+#   r<-sample(nrow(IND),size=138,replace=TRUE)
+#   m<-IND[r,]
+#   m<-m[,c(5,6,8)]
+# 
+#   model<-svm(TPGrp~.,data=m,kernel="linear",cost=1,scale=FALSE,class.weights=(c("high"=4,"low"=1))) 
+#   pred<-predict(model,m)
+#   hh<-table(pred=pred,true=m$TPGrp)[1]# confusion matrix
+#   hl<-table(pred=pred,true=m$TPGrp)[2]
+#   lh<-table(pred=pred,true=m$TPGrp)[3]
+#   ll<-table(pred=pred,true=m$TPGrp)[4]
+#   confoutput[k,]<-as.matrix(cbind(hh,hl,lh,ll))
+# 
+# }
 
 ##Get the slope and intercept of the hyperplane (line)
 ##and the upper margin and the lower margin of the decision boundary
@@ -46,7 +69,7 @@ mindx<-as.data.frame(x[model$index,])#support points that determine the boundary
 
 ##Plot the hyperplane
 ggplot()+
-  geom_point(data=IND,aes(H,L,colour=factor(TPGrp)))+
+  geom_point(data=IND,aes(H,L,colour=factor(TPGrp3)))+
   #geom_point(data=mindx,aes(H,L),shape=21,size=5)+#support points that determine the boundary
   xlim(0,1)+
   ylim(0,1)+
@@ -56,7 +79,7 @@ ggplot()+
   geom_abline(intercept=int2,slope=sl,linetype=2)+
   labs(x="Rel Abund Tolerant Diatoms",
        y="Rel Abund Senstitive Diatoms",
-       colour="TP GRP")
+       colour="TP GRP3")
 
 
 
