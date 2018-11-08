6c3784927c6b965fa285ac1cb5b7a92f56de1e1b
marybecker <mary.becker@ct.gov>
2018-11-08 16:45:19 -0500

    generate list of imp sites

--- svm.R
+++ svm.R
@@ -1,10 +1,10 @@
-setwd("/Users/tbecker/Documents/Projects/GitHubProjects/DiatomMetricThreshold/data")
+setwd("P:/Projects/GitHub_Prj/DiatomMetricThreshold/data")
 
 library(ggplot2)
 library(e1071)
 
 IND <-  read.csv ("DiatomMetrics_GAM.csv",sep=",",header=TRUE)
-IND<-IND[IND$ID!='S18882_2015',]
+#IND<-IND[IND$ID!='S18882_2015',]
 IND[is.na(IND)] <- 0
 IND$TPGrp<-factor(ifelse(IND$TP_MGL<0.065,"low","high"))
 IND$TPGrp3<-factor(ifelse(IND$TP_MGL<=0.02,"low",
@@ -32,6 +32,36 @@ table(pred=pred,true=m$TPGrp)# confusion matrix
 m2<-INDT[,c(5,6,7)]
 pred2<- predict(model,m2)
 table(pred=pred2,true=m2$TPGrp)# confusion matrix
+
+##Combine Calibration and Test Predictions Together
+predsites<-as.data.frame(pred)
+predsites<-cbind(predsites,IND)
+predsites<- predsites[,c(1,3:7,9)]
+colnames(predsites)[2]<-"ID"
+
+predsitesT<-as.data.frame(pred2)
+predsitesT<-cbind(predsitesT,INDT)
+predsitesT<-predsitesT[,c(1:4,6:8)]
+colnames(predsitesT)[1]<-"pred"
+colnames(predsitesT)[3]<-"Station_Name"
+predsites<-rbind(predsites,predsitesT)
+predsites$ImpairT<-ifelse(predsites$pred=="high"&predsites$TPGrp=="high",1,0)
+
+library(RODBC)
+
+cn<- odbcDriverConnect("Driver=ODBC Driver 13 for SQL Server; Server= DEEPDBS120; Database=WQX;
+                         Uid=wqx_readonly;Pwd=wqx_readonly")
+sites<- sqlFetch(cn,"Stations")
+
+odbcClose(cn)
+
+sites<-sites[,c(1,11:12)]
+colnames(sites)[1]<-"ID"
+predsites<-merge(predsites,sites,by.x="ID")
+write.csv(predsites,"predsites.csv")
+
+
+
 
 ##Run Linear SVM Model with Bootstrap
 
@@ -73,20 +103,22 @@ mindx<-as.data.frame(x[model$index,])#support points that determine the boundary
 
 ##Plot the hyperplane
 ggplot()+
-  geom_point(data=IND,aes(H,L,colour=factor(TPGrp)))+
+  geom_point(data=IND,aes(H,L,colour=factor(TPGrp3)))+
   #geom_point(data=mindx,aes(H,L),shape=21,size=5)+
   #support points that determine the boundary
   xlim(0,1)+
   ylim(0,1)+
-  theme(legend.position=c(0.75,0.75))+
-  geom_abline(intercept = int,slope=sl)+
-  geom_abline(intercept=int1,slope=sl,linetype=2)+
-  geom_abline(intercept=int2,slope=sl,linetype=2)+
+  theme(legend.position=c(0.75,0.75),axis.title=element_text(size=rel(1.5)),
+        legend.text=element_text(size=rel(1.5)),
+        legend.title=element_text(size=rel(1.5)))+
+  geom_abline(intercept = int,slope=sl,size=2)+
+  #geom_abline(intercept=int1,slope=sl,linetype=2)+
+  #geom_abline(intercept=int2,slope=sl,linetype=2)+
   labs(x="Rel Abund Tolerant Diatoms",
        y="Rel Abund Senstitive Diatoms",
-       colour="TP GRP")
+       colour="TP Concentration")
 
-
+ggsave("svm_diatom.tiff",height=5,width=5,units="in")
 
 
 ggplot(IND,aes(H,TP_MGL))+
